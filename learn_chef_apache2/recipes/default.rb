62e6e50b31abca522f44b5f19fc03e26130bb6ce
shlyambur <shlyambur@gmail.com>
2016-05-15 16:17:03 +0300

    Update default.rb

--- learn_chef_apache2/recipes/default.rb
+++ learn_chef_apache2/recipes/default.rb
@@ -10,6 +10,6 @@ service 'apache2' do
 	action [:enable, :start]
 end
 
-template 'var/www/html/index.html' do
-	source 'index.html.erb'
-end
+#template 'var/www/html/index.html' do
+#	source 'index.html.erb'
+#end
